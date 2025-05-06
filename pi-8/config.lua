3befdf01952848af6faacac533cd75f22fdac023
ProgramWithLukas <ethan.custodiio3@gmail.com>
2025-05-06 14:28:51 +0800

    Update config.lua

--- pi-8/config.lua
+++ pi-8/config.lua
@@ -5,11 +5,15 @@ function conf.AddOBJ(t)
   table.insert(sc.cnt, t)
 end
 
+function conf:SetPlatform(p)
+  sc.platform = p
+end
+
 function conf:screen(w, h)
   sc.width = w; sc.height = h
 end
 
-function conf:NEG()
+function conf:SetToRAW()
   os.execute("stty raw -echo")
 end
 
