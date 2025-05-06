65bd2eea2c0f57a2bec47765c6346acc574d63d3
ProgramWithLukas <ethan.custodiio3@gmail.com>
2025-05-06 14:26:46 +0800

    Update time.lua

--- pi-8/time.lua
+++ pi-8/time.lua
@@ -1,8 +1,13 @@
 local t = {}
 
 function t:WaitForTime(num)
-  local c = require 'Pi-8/terminal'
-  c:Command("sleep " .. tostring(num))
+  local c = require 'pi-8/terminal'
+  local sc = require 'pi-8/sc'
+  if sc.platform == "LINUX" then
+    c:Command("sleep " .. tostring(num))
+  elseif sc.platform == "WINDOWS" then
+    c:Command("timeout /t " .. tostring(num) .. " >nul")
+  end
 end
 
 return t
